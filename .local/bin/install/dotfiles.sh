8ccbb944819c251fd7fcdbb6e0e8cad7f2cb93bf
Christopher Cain <clcain@pm.me>
2024-12-28 22:19:01 -0500

    Add dotfiles config installer and fix fish installer
    This adds some config items to install so that when you use
    dotfiles-git, it doesn't show untracked items (which there will be many)
    and it will also not use the untracked files in the autocompletion
    options for both bash and fish, which really slows down the shell as it
    figures out all nearly 3000 options.

---
+++ .local/bin/install/dotfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Disable showing the untracked files in both git status and the bash/fish autocompletions while using dotfiles-git
+dotfiles-git config --local status.showUntrackedFiles no
+dotfiles-git config --local bash.showUntrackedFiles false
--- .local/bin/install/fish.sh
+++ .local/bin/install/fish.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 chsh -s /usr/bin/fish
