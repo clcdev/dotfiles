e877f00c48626aeb784fd5a44859265d0739b342
Christopher Cain <clcain@pm.me>
2024-12-28 21:32:38 -0500

    Add initial fish configuration

---
+++ .config/fish/conf.d/010-vars.fish
@@ -0,0 +1,17 @@
+fish_add_path -g ~/.local/bin
+
+# Set XDG basedirs.
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
+set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
+set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
+set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
+for xdgdir in (path filter -vd $XDG_CONFIG_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_CACHE_HOME)
+    mkdir -p $xdgdir
+end
+
+# Set up our variables
+set -q GITHUB_USERNAME; or set -gx GITHUB_USERNAME clcdev
+set -q EDITOR; or set -gx EDITOR helix
+set -q SSH_ASKPASS; or set -gx SSH_ASKPASS $HOME/.local/bin/hypr-askpass
+set -q SSH_ASKPASS_REQUIRE; or set -gx SSH_ASKPASS_REQUIRE prefer
---
+++ .config/fish/conf.d/020-aliases.fish
@@ -0,0 +1,3 @@
+
+alias dotfiles-git='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias dotfiles-lazy='/usr/bin/lazygit -g $HOME/dotfiles/ -w $HOME'
