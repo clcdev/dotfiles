d62f132ce7462fa19c78b43a0fbb6d8c9ef6833a
Christopher Cain <clcain@pm.me>
2025-01-01 15:42:48 -0500

    Add lualine and neo-tree to nvim and move more things into separate files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,26 @@ local M = {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "c_sharp",
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "html",
+        "java",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true }, 
+    })
+  end
 }
 
 return { M }
